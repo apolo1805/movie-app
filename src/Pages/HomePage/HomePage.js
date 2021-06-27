4d69353ff521d63db22b71bf3751a2b83fb60e56
elad <eladlevavi@gmail.com>
2021-06-27 21:59:21 +0300

    Fix movies page

--- src/Pages/HomePage/HomePage.js
+++ src/Pages/HomePage/HomePage.js
@@ -1,10 +1,18 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
+import './HomePage.css';
 
 function HomePage() {
     return (
         <div className="p-home">
             <h1>Home Page</h1>
-            
+            <p>
+                Go to <Link to="/actors">Actors Page</Link><br/>
+            </p>
+
+            <p>
+                Go to <Link to="/movies">Movies Page</Link>
+            </p>
         </div>
     );
 }
--- src/Pages/MoviesPage/MoviesPage.js
+++ src/Pages/MoviesPage/MoviesPage.js
@@ -1,10 +1,29 @@
+import axios from 'axios';
 import React from 'react';
+import { Button } from 'react-bootstrap';
 import './MoviesPage.css';
 
 function MoviesPage() {
+    const [movies, setMovies] = React.useState([]);
+
+    const apiKey = "ca0fa9f32ca8d7ab8daf415172adb490";
+    const movieImgPrefix = "https://image.tmdb.org/t/p/w200";
+
+
+    React.useEffect(() => {
+        axios.get("https://api.themoviedb.org/3/movie/popular?api_key=" + apiKey).then(response => {setMovies(response.data.results)})}, []);
+
     return (
         <div className="p-movies">
-            <h3>Movies Page</h3>
+            <Button onClick={router.history}>Back</Button>
+            <h1>Movies Page</h1>
+            {
+                movies.map(movie => 
+                    <>
+                        <h3>{movie.title}</h3>
+                        <img src={movieImgPrefix + movie["poster_path"]}/>
+                    </>)
+            }
         </div>
     );
 }
